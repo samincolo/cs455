8e3541d1068ba2609248ba7ef4a290d094153008
JeanMarc <37602279+Marc-9@users.noreply.github.com>
2021-05-03 14:02:39 -0700

    Website Changes
    Site works
    https://skillsadvisor.net

--- local_html/api/index.php
+++ local_html/api/index.php
@@ -1,5 +1,5 @@
 <?php
-require('../config.php');
+require('../../config.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -26,8 +26,15 @@ if($requestType == "GET"){
 			array_push($ids,$result['id']);
 		}
 		$stmt->close();
-		
+		$jsonRes = [];
+
+		findCoorelations($ids, $cityId, $db, $jsonRes);
+
 		$intersection = [];
+		$strict = false;
+		if($_GET['strict'] == "true"){
+			$strict = true;
+		}
 		foreach($ids as $kword){
 			$keyArray = [];
 			$jobIds = $db->query("SELECT jobId FROM job_keyword WHERE cityId = $cityId AND keywordId = $kword");
@@ -38,21 +45,120 @@ if($requestType == "GET"){
 				$intersection = $keyArray;
 			}
 			else{
-				$intersection = array_intersect($intersection, $keyArray);
+				if($strict){
+					$intersection = array_intersect($intersection, $keyArray);
+				}
+				else{
+					$intersection = array_unique(array_merge($intersection, $keyArray));
+				}
 			}
 		}
 
-		$jsonResponse = "[";
+		$jsonResponse = "[[";
 		foreach($intersection as $jobSearch){
 			$jsonResponse .= "{";
 			$jobInfo = mysqli_fetch_assoc($db->query("SELECT * FROM jobs WHERE id = $jobSearch"));
-			$jsonResponse .= '"title": "'.$jobInfo['jobTitle'].'","company": "'.$jobInfo['company'].'", "url": "'.$jobInfo['url'].'", "lat": "'.$jobInfo['lat'].'", "lng": "'.$jobInfo['lng'].'"},';
+			$jsonResponse .= '"title": "'.$jobInfo['jobTitle'].'","company": "'.$jobInfo['company'].'", "url": "'.$jobInfo['url'].'", "lat": "'.$jobInfo['lat'].'", "lng": "'.$jobInfo['lng'].'", "id": "'.$jobInfo['id'].'"},';
 
 		}
 		$jsonResponse = rtrim($jsonResponse, ", ");
-		$jsonResponse .= "]";
+		$jsonResponse .= "],".json_encode($jsonRes)."]";
 		echo $jsonResponse;
 
 
 	}
+
+	if($_GET['url'] == 'getJobDesc'){
+		$stmt = $db->prepare("SELECT description FROM jobs WHERE id = ?");
+		$stmt->bind_param("i", $_GET['id']);
+		$stmt->execute();
+		$jsonResponse = $stmt->get_result()->fetch_assoc();
+		$stmt->close();
+		echo json_encode($jsonResponse);
+	}
+
 }
+
+function findCoorelations($ids, $cityid, $db, &$return){
+	$keywords = array();
+	idsAndNames($ids, $keywords);
+
+	$counter = 0;
+	foreach($ids as $id){
+		$result = returnCorrelated($db, $id, $cityid);
+		$avgArray = [];
+		$return[$keywords[$counter]] = array();
+		while($find = $result->fetch_assoc()){
+			$kname = mysqli_fetch_assoc($db->query("SELECT name FROM keywords WHERE id = $find[coorelatedId]"))['name'];
+			if(isset($avgArray[$kname])){
+				array_push($avgArray[$kname], $find['coorelationValue']);
+			}
+			else{
+				$avgArray[$kname] = [$find['coorelationValue']];
+			}
+			
+		}
+		foreach ($avgArray as $key => $value) {
+			$temp = [$key, (array_sum($avgArray[$key]) / count($avgArray[$key]) ) ];
+			array_push($return[$keywords[$counter]], $temp);
+		}
+		$counter += 1;
+	}
+	
+
+
+}
+
+function returnCorrelated($db, $id, $cityid){
+	$result = $db->query("SELECT coorelatedId, coorelationValue FROM correlations WHERE keywordsSum = $id and cityId = $cityid AND coorelationValue < 1 ORDER BY coorelationValue DESC LIMIT 3");
+	if(mysqli_num_rows($result) == 0){
+		$result = $db->query("SELECT coorelatedId, coorelationValue FROM correlations WHERE keywordsSum = $id  AND coorelationValue < 1 ORDER BY coorelationValue DESC LIMIT 3");
+	}
+	if(mysqli_num_rows($result) == 0){
+		$result = $db->query("SELECT coorelatedId, coorelationValue FROM correlations WHERE keywordsSum = $id  AND coorelationValue ORDER BY coorelationValue DESC LIMIT 3");
+	}
+	return $result;
+}
+
+function permute($arr, $temp_string, &$permutedIds) {
+    if ($temp_string != "") 
+        $permutedIds []= $temp_string;
+
+    for ($i=0, $iMax = sizeof($arr); $i < $iMax; $i++) {
+        $arrcopy = $arr;
+        $elem = array_splice($arrcopy, $i, 1);
+        if (sizeof($arrcopy) > 0) {
+            permute($arrcopy, (int)$temp_string + $elem[0], $permutedIds);
+        } else {
+            $permutedIds []= $temp_string + $elem[0];
+        }   
+    }   
+}
+
+function idsAndNames(&$keyIds, &$keyNames){
+
+	$keywordNameToIds = json_decode('{"expressjs":"4398046511104","tensorflow":"2199023255552","kotlin":"1099511627776","bash":"549755813888","docker":"274877906944","spark":"137438953472","hadoop":"68719476736","azure":"34359738368","aws":"17179869184","perl":"8589934592","laravel":"4294967296","vba":"2147483648","linux":"1073741824","django":"536870912","swift":"268435456","ruby":"134217728","asp.net":"67108864","node.js":"33554432","r":"16777216","ios":"8388608","mysql":"4194304","css":"2097152","jquery":"1048576","html":"524288","android":"262144","lisp":"131072","mongo":"65536","nosql":"32768","clojure":"16384","haskell":"8192","scala":"4096","rust":"2048","c#":"1024","c++":"512","c":"256","golang":"128","python":"64","php":"32","java":"16","vue":"8","reactjs":"4","angular":"2","javascript":"1"}', true);
+
+
+	if(!(count($keyIds) == 1)){
+
+		$permutedIds = array();
+		permute($keyIds, "", $permutedIds);
+
+		$keyIds = array_unique($permutedIds);
+	}
+
+	foreach($keyIds as $entry){
+	    $entrycopy = $entry;
+	    $keywords = "";
+	    foreach ($keywordNameToIds as $key => $value) {
+	        if($entrycopy - $value >= 0){
+	            $entrycopy -= $value;
+	            $keywords .= $key.'&';
+	        }
+	    }
+	    $keywords = rtrim($keywords, "&");
+	    array_push($keyNames, $keywords);
+	}
+}
+
--- local_html/index.php
+++ local_html/index.php
@@ -7,18 +7,20 @@
     <title>455</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+
     <link rel="stylesheet" href="assets/css/Add-Another-Button.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="assets/css/Multi-step-form.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-typeahead/2.11.0/jquery.typeahead.min.css">
+
 </head>
 
 <body style="background: var(--gray-dark);">
     <h1 class="text-center" style="color: var(--light);">Skill Finder</h1>
     <section>
-        <div id="multple-step-form-n" class="container" style="margin-top: 0px;margin-bottom: 10px;padding-top: 57px;">
+        <div id="multple-step-form-n" style="margin-top: 0px;margin-bottom: 10px;padding-top: 57px;">
             <div id="progress-bar-button" class="multisteps-form">
                 <div class="row">
                     <div class="col-12 col-lg-8 ml-auto mr-auto mb-4">
@@ -31,6 +33,7 @@
                     <form id="main-form" class="multisteps-form__form">
                         <div id="single-form-next" class="multisteps-form__panel shadow p-4 rounded bg-white js-active" data-animation="scaleIn">
                             <h3 class="text-center multisteps-form__title">Enter your City</h3>
+
                             <div id="form-content" class="multisteps-form__content">
                                 <div id="input-grp-single" class="form-row mt-4">
                                     <div class="col-12">
@@ -49,7 +52,13 @@
                                     <div class="col-12"><input class="form-control multisteps-form__input skillSearch" type="text" placeholder="Skill 1"></div>
                                 </div>
                                 <br>
+
                                 <button id='addButton' class="btn btn-outline-primary text-truncate float-none float-sm-none add-another-btn" data-bss-hover-animate="pulse" type="button">Add Skill<i class="fas fa-plus-circle edit-icon"></i></button>
+                            
+                                <div class="form-check">
+								    <input type="checkbox" class="form-check-input" id="strict">
+								    <label class="form-check-label" for="exampleCheck1">Include Only Jobs that match ALL keywords</label>
+								  </div>
                                 <div id="next-prev-buttons" class="button-row d-flex mt-4">
                                 	<button class="btn btn btn-primary js-btn-prev" type="button" title="Prev">Prev</button>                            
                                 	<button class="btn btn btn-primary ml-auto js-btn-next" id='getResults' onclick="getJobs()" type="button" title="Next">Find Results</button>
@@ -57,10 +66,15 @@
                             </div>
                         </div>
                         <div id="single-form-next-prev-1" class="multisteps-form__panel shadow p-4 rounded bg-white" data-animation="scaleIn">
-                            <h3 class="text-center multisteps-form__title">Map</h3>
-                            <div id="form-content-2" class="multisteps-form__content">
+                            <h3 class="text-center multisteps-form__title">Results</h3>
+                            	<img class="centered" id="loading" style="display:none" src="load.gif">
+                            	<div id='myDiv'><!-- Plotly chart will be drawn inside this DIV --></div>
+				          		<br>
                                 <div id="mapid"></div>
-                                <div id="next-prev-buttons-1" class="button-row d-flex mt-4"><button class="btn btn btn-primary js-btn-prev" type="button" title="Prev">Prev</button><button class="btn btn btn-primary ml-auto js-btn-next" type="button" title="Next">Next</button></div>
+                                <div id="next-prev-buttons-1" class="button-row d-flex mt-4"><button class="btn btn btn-primary js-btn-prev" type="button" id="jump" title="Prev">Prev</button></div>
+                                <h4 class="text-center">Click Marker to view job description</h4>
+                                <div id="description">
+    							</div>
                             </div>
                         </div>
                     </form>
@@ -68,6 +82,7 @@
             </div>
         </div>
     </section>
+    
     
 
     <script src="assets/js/jquery.min.js"></script>
@@ -77,43 +92,139 @@
     <script src="assets/js/Multi-step-form.js"></script>
     <script src="assets/js/leaflet2.js"></script>
     <script src="assets/js/leaflet.js"></script>
+    <script src='https://cdn.plot.ly/plotly-latest.min.js'></script>
+
+
 
     <script>
-    	L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
-    </script>
+    	var layerGroup = L.featureGroup().addTo(map).on("click", groupClick);
+    	$( "#jump" ).click(function() {
+    		document.getElementById("description").innerHTML = "";
+  $('html,body').scrollTop(0);
+});
 
-    <script>
     	function getJobs(){
+    		layerGroup.clearLayers();
+    		Plotly.purge("myDiv");
+    		$( "#loading" ).toggle();
     		let city = document.getElementById("citySearch").value;
+    		let strict = document.getElementById("strict").checked;
     		let keywords = [];
     		var x = document.getElementsByClassName("skillSearch");
     		for (var i = 0; i < x.length; i++) {
     			if(x[i].value.length > 0){
- 				 keywords.push(x[0].value)
+ 				 keywords.push(x[i].value)
     			}
 			}
 
-    	$.ajax({
-           type: "GET",
-           url: "api/getJobs",
-           data: {"city": city, "keywords": keywords.join()},
-           success: function(data)
-           {
-           	populateLeaflet(data);
-           }
-         });
-    	}
+
+	    	$.ajax({
+	           type: "GET",
+	           url: "api/getJobs",
+	           data: {"city": city, "keywords": keywords.join(), "strict" : strict},
+	           success: function(data)
+	           {
+	           	$( "#loading" ).toggle();
+	           	let json = JSON.parse(data);
+
+	           	populateLeaflet(json[0]);
+	           	populateSunburst(json[1], keywords.length);
+	           }
+	         });
+	    	}
 
     	function populateLeaflet(data){
-    		console.log(data);
-    		let json = JSON.parse(data);
-    		map.flyTo([json[0]['lat'],json[0]['lng']], 12);
-    		json.forEach(element =>
-    			L.marker([element['lat'], element['lng']]).addTo(map).bindPopup(element['title'])
-    			)
+    		if(data.length == 0){
+    			alert("No Jobs found with all of the keywords listed");
+    			return;
+    		}
+    		map.flyTo([data[0]['lat'],data[0]['lng']], 12);
+
+    		var marker;
+    		data.forEach(element =>{
+    			marker = L.marker([element['lat'], element['lng']]).addTo(layerGroup).bindPopup("<h4>"+element['title']+"</h4><h6>"+element['company']+"<br><a href='"+element['url']+"'>Job Link</a></h6>")
+    			marker.id = element['id']
+    			})
     	}
+
+    	function populateSunburst(data, numSkills){
+    			let count = 1;
+
+				Object.keys(data).forEach(item => count += data[item].length + 1);
+
+				values1 = [0];
+				labels1 = ["Click keywords<br>to expand"];
+				parents1 = [""];
+				maxdepth = 3;
+				if(numSkills > 3){
+					maxdepth = 2;
+				}
+				Object.keys(data).forEach(item => {
+							labels1.push(item);
+							parents1.push(0);
+							let sum = 0;
+							data[item].forEach(element => {
+								sum += element[1];
+							})
+							values1.push(sum / data[item].length);
+						
+					}
+					);
+				Object.keys(data).forEach(item => {
+						data[item].forEach(element => {
+							labels1.push(element[0]);
+							parents1.push(labels1.indexOf(item));
+							values1.push(element[1]);
+						})
+						
+					}
+					);
+				
+			
+
+				var data = [{
+					maxdepth: maxdepth,
+					type: "sunburst",
+					ids: [...Array(count).keys()],
+					labels: labels1,
+					parents: parents1,
+					values: values1,
+					outsidetextfont: {size: 20, color: "#377eb8"},
+					leaf: {opacity: 0.4},
+					marker: {line: {width: 2}},
+				}];
+
+			var layout = {
+				margin: {l: 0, r: 0, b: 0, t: 0},
+				sunburstcolorway:["#636efa","#ef553b","#00cc96", "#ed63fa","#3182bd", "#e6550d", "#9ecae1", "#fdae6b", "#9ecae1",
+             "#fdae6b", "#deebf7", "#deebf7", "#deebf7", "#fee6ce"],
+				};
+			var config = {responsive: true}
+
+
+			Plotly.newPlot('myDiv', data, layout, config);
+
+    	}
+
+    	function groupClick(event) {
+    		$.ajax({
+	           type: "GET",
+	           url: "api/getJobDesc",
+	           data: {"id": event.layer.id},
+	           success: function(data)
+	           {
+	           	let json = JSON.parse(data);
+
+	           	document.getElementById("description").innerHTML = json['description'];
+
+	           }
+	         });
+		}
+
+
+
+
+
     </script>
 </body>
-
 </html>
