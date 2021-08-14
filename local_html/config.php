e1be70a68251e9fea885055ae4d20dbc8c4245e7
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-08-14 08:20:25 -0600

    why is it so hard to ignore files

--- local_html/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!defined('DB_HOST')) define('DB_HOST', '');
-if (!defined('DB_USER')) define('DB_USER', '');
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
-if (!defined('DB_DATABASE')) define('DB_DATABASE', '');
-if (!defined('DB_DATABASE1')) define('DB_DATABASE1', '');
-$url = "http://localhost/";
-?>
