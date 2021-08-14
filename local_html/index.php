9937fdd89e272d70fea1a465d2732974fd875832
Jean-Marc Ruffalo-Burgat <me@jeanmarc.us>
2021-08-14 09:57:49 -0600

    add modal for advanced Options

--- local_html/index.php
+++ local_html/index.php
@@ -46,6 +46,25 @@
             </div>
         </div>
     </section>
+    <div class="modal fade" id="advancedOptions" tabindex="-1" role="dialog" aria-labelledby="advancedOptions" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="advancedOptionsLabel">Advanced Options</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     
     
 
